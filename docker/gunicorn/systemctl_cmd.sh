9d8a412339f91b86b1257857d44b78b8157a34ec
devspoon <devspoon@naver.com>
2020-04-23 22:52:42 +0900

    django static option in nginx.conf

--- docker/gunicorn/systemctl_cmd.sh
+++ docker/gunicorn/systemctl_cmd.sh
@@ -6,8 +6,11 @@ do
     result=$(ps -ax|grep -v grep|grep systemd-journald)
     if [ "${#result}" -ne 0 ]
     then
+        sleep 1
         echo 'systemctl daemon-reload'
+        sleep 1
         echo 'systemctl start gunicorn'
+        sleep 1
         echo 'systemctl enable gunicorn'
         break
     else
