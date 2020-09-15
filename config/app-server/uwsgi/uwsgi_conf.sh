fec97081814ca91a48705d8a2e9e8d78878b2d52
devspoon <devspoon@naver.com>
2020-09-15 16:17:21 +0900

    nginx config file update from used case of uwsgi_param to proxy_pass. after updated uwsgi, some parameter working has problem. to avoid new problem like these issues, it will handle like revers proxy config style. it is working as well in tests. compose, uwsgi.ini files updated and added create script for making uwsgi.ini file.

---
+++ config/app-server/uwsgi/uwsgi_conf.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+while :
+do 
+    echo -n "Enter the project path >"
+    echo -n "* if your project path has sub-level, you should be insert as \\\/A\\\/B\\\/C"
+    read project_path
+    echo  "Entered the project path: $project_path"
+    if [[ "$project_path" != "" ]]; then
+        break
+    fi
+done 
+
+while :
+do 
+    echo -n "Enter the project name >"
+    read project_name
+    echo  "Entered the project name: $project_name"
+    if [[ "$project_name" != "" ]]; then
+        break
+    fi
+done 
+
+p_num=$(grep -c processor /proc/cpuinfo)
+
+if [[ "$p_num" == "" ]]; then
+    echo "this job can't access /proc/cpuinfo. so process number be setting as 4."
+    p_num=4
+fi
+
+th_num=$p_num
+
+while :
+do 
+    echo -n "Enter the service port number >"
+    read port_num
+    echo  "Entered the service port number: $port_num"
+    if [[ "$port_num" != "" ]]; then
+        break
+    fi
+done 
+
+sed 's/project_path/'$project_path'/g' sample_uwsgi.ini > $project_name'1'.temp
+sed 's/project_name/'$project_name'/g' $project_name'1'.temp > $project_name'2'.temp
+sed 's/p_num/'$p_num'/g' $project_name'2'.temp > $project_name'3'.temp
+sed 's/th_num/'$th_num'/g' $project_name'3'.temp > $project_name'4'.temp
+sed 's/port_num/'$port_num'/g' $project_name'4'.temp > uwsgi.ini
+
+rm *.temp
