246765f1140d16b0d67a6b7e5050a2a4e4822102
devspoon <devspoon@naver.com>
2020-09-15 16:20:41 +0900

    bug fix

--- config/app-server/uwsgi/uwsgi_conf.sh
+++ config/app-server/uwsgi/uwsgi_conf.sh
@@ -2,8 +2,8 @@
 
 while :
 do 
-    echo -n "Enter the project path >"
     echo -n "* if your project path has sub-level, you should be insert as \\\/A\\\/B\\\/C"
+    echo -n "Enter the project path >"
     read project_path
     echo  "Entered the project path: $project_path"
     if [[ "$project_path" != "" ]]; then
